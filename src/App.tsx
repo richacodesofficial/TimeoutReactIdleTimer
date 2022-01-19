775c814be5a9c1dc4b768ab09c5a8e0471ad6ffc
richacodesofficial <38663152+richacodesofficial@users.noreply.github.com>
2022-01-19 11:20:28 +0530

    Create App.tsx

---
+++ src/App.tsx
@@ -0,0 +1,70 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+import React, { useState, useEffect } from 'react'
+import { format } from 'date-fns'
+import { useIdleTimer } from 'react-idle-timer'
+
+export default function App () {
+  const timeout = 500000000;
+  const [remaining, setRemaining] = useState(timeout)
+  const [elapsed, setElapsed] = useState(0)
+  const [lastActive, setLastActive] = useState(+new Date())
+  const [lastEvent, setLastEvent] = useState('Events Emitted on Leader')
+  const [leader, setLeader] = useState(true)
+
+  const handleOnActive = () => setLastEvent('active')
+  const handleOnIdle = () => setLastEvent('idle')
+
+  const {
+    reset,
+    pause,
+    resume,
+    getRemainingTime,
+    getLastActiveTime,
+    getElapsedTime,
+    isIdle,
+    isLeader
+  } = useIdleTimer({
+    timeout,
+    //onActive: handleOnActive,
+    onIdle: handleOnIdle,
+    // crossTab: {
+    //   emitOnAllTabs: true
+    // }
+  })
+
+  const handleReset = () => reset()
+  const handlePause = () => pause()
+  const handleResume = () => resume()
+
+  useEffect(() => {
+    setRemaining(getRemainingTime())
+    setLastActive(getLastActiveTime())
+    setElapsed(getElapsedTime())
+
+    setInterval(() => {
+      setRemaining(getRemainingTime())
+      setLastActive(getLastActiveTime())
+      setElapsed(getElapsedTime())
+      setLeader(isLeader())
+    }, 1000)
+  }, [])
+
+  return (
+    <div>
+      <div>
+        {/* <h1>Timeout: {timeout}ms</h1> */}
+        <h1>Time Remaining: {remaining}</h1>
+        {/* <h1>Time Elapsed: {elapsed}</h1> */}
+        {/* <h1>Last Active: {format(lastActive, 'MM-dd-yyyy HH:MM:ss.SSS')}</h1> */}
+        <h1>Last Event: {lastEvent}</h1>
+        {/* <h1>Is Leader: {leader.toString()}</h1> */}
+        {/* <h1>Idle: {isIdle().toString()}</h1> */}
+      </div>
+      <div>
+        <button onClick={handleReset}>RESET</button>
+        <button onClick={handlePause}>PAUSE</button>
+        <button onClick={handleResume}>RESUME</button>
+      </div>
+    </div>
+  )
+}
